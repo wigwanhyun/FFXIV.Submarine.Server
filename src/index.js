4ea51fd58751fc5abdb6dbdcd8e37505680381bb
wie.gh <dnlrhksgus0511@gmail.com>
2021-06-21 23:41:48 +0900

    test

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ app.get("/", (req, res) => {
 });
 
 app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://puzzle.girin.dev");
+    res.header("Access-Control-Allow-Origin", "https://puzzle.girin.dev/");
     res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
     res.header('Access-Control-Allow-Credentials', 'true'); 
     next();
